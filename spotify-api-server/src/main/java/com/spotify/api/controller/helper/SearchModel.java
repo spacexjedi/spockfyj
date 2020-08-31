2ff88f834adad4fbfdca2c262772bfb75883cffe
th3exorcist <ghostsavestheworld@gmail.com>
2020-08-30 22:02:28 -0300

    sprint 1 backend api

---
+++ spotify-api-server/src/main/java/com/spotify/api/controller/helper/SearchModel.java
@@ -0,0 +1,35 @@
+package com.spotify.api.controller.helper;
+
+
+public class SearchModel {
+
+    private String query;
+
+    private int limit = 20;
+
+    private int offset = 0;
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+}
